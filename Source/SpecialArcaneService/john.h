5a6767a2b9f0938e502540e0a5544e34d56d7a11
beebs <usz.tv5@icloud.com>
2025-11-13 13:21:51 -0700

    Reapply "Continue working on ice shard"
    This reverts commit 0b21ca84f153d674ae4458795bc0c23ab1aedf57.

---
+++ Source/SpecialArcaneService/john.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Elements/Actor/ActorElementHierarchyInterface.h"
+#include "john.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPECIALARCANESERVICE_API Ujohn : public UActorElementHierarchyInterface
+{
+	GENERATED_BODY()
+	
+};
