08b3f9caa6fbd8705fc2afee109ba91724a28780
kiëst <147881592+kiestdev@users.noreply.github.com>
2026-03-03 12:47:14 -0700

    Set default volume to 1.0f

---
+++ Source/SpecialArcaneService/Public/SASConsole.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/Console.h"
+#include "SASConsole.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPECIALARCANESERVICE_API USASConsole : public UConsole
+{
+	GENERATED_BODY()
+	
+};
